734b472abde3d8584caa4be88620d393ed55b543
CorfuAlin94 <corfualinconstantin@gmail.com>
2023-04-02 23:00:38 +0300

    02.04.2023

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,12 +1,44 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { set } from "react-native-reanimated";
 
 const TextScreen = () => {
+  const [text, setText] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log(text);
   return (
     <View>
-      <Text>Text Screen</Text>
+      <Text>Enter Name: </Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={text}
+        onChangeText={(newValue) => setText(newValue)}
+      />
+      <Text>My name is: {text}</Text>
+      <Text>Enter password: </Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={(newPassword) => setPassword(newPassword)}
+      />
+      {password.length < 4 ? (
+        <Text>Password must be at least 5 char. </Text>
+      ) : null}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+});
 
 export default TextScreen;
